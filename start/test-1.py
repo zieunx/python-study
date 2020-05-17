52d8c8a392afe0a4be51e489121ca10c74d9f1d4
zieunx <wldms_1217@naver.com>
2020-05-18 00:50:19 +0900

    파이썬 시작하기 (if문, for문, while문)

---
+++ start/test-1.py
@@ -0,0 +1,39 @@
+money = 2000
+card = False
+
+if money >= 3000 or card:
+    print('택시를 타고 가라.')
+elif money >= 2000:
+    print('버스를 타라.')
+else:
+    print('걸어 가라.')
+
+if 1 not in [1, 2, 3]:
+    print('1이 없다.')
+else:
+    print('1이 있다.')
+
+
+score = 50
+if score >= 60:
+    message = "success"
+else:
+    message = "failure"
+
+# 조건부 표현식 (위의 if문과 동일.)
+message = "success" if score >= 60 else "failure"
+print('\n\n')
+
+# while문으로 별 찍기
+n = 1
+while n <= 5:
+    print('*' * n)
+    n = n + 1
+
+# for문으로 별 찍기
+for i in range(5):
+    print('*' * (i+1))
+
+# for문으로 별 찍기 (오른쪽 정렬)
+for i in range(5):
+    print('{:>5}'.format('*' * (i+1)))
